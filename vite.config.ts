8325031d9cc965c55afd7efeaafa5e38cc7382a5
DE-Uehara <r.uehara@defide-ix.com>
2025-04-10 21:59:01 +0900

    viteでSSL化

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // httpsを無効にする設定
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), basicSsl()],
   server: {
+    https: {
+      minVersion: 'TLSv1.2'
+    },
     host: '0.0.0.0',
     port: 3000,
     cors: true
