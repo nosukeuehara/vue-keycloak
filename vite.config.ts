e643adb2400b06558b9d5d1a7916e116ac300fd9
DE-Uehara <r.uehara@defide-ix.com>
2025-04-10 21:45:44 +0900

    修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// httpsを無効にする設定
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '0.0.0.0',
     port: 3000,
     cors: true
   }
